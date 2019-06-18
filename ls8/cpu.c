466f3d0398717d0a43b68aac45e40b5263149f21
Leianne Louis <leilouis112@gmail.com>
2019-06-18 11:14:51 -0400

    updated PRN

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -45,17 +45,41 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
  */
 void cpu_run(struct cpu *cpu)
 {
+  int operandA;
+  int operandB;  
   int running = 1; // True until we get a HLT instruction
-
+  unsigned char IR;
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     int PC = cpu->PC;
+    IR = cpu->RAM[PC];
     // 2. Figure out how many operands this next instruction requires
-    int operandA, operandB = cpu_ram_read(cpu);
+    int num_of_operands = cpu_ram_read(cpu);
     // 3. Get the appropriate value(s) of the operands following this instruction
+    if ( num_of_operands == 2) {
+      operandA = cpu->RAM[(cpu->PC) + 1];
+      operandB = cpu->RAM[(cpu->PC) + 2];
+    } else if (num_of_operands == 1) {
+      operandA = cpu->RAM[(cpu->PC) + 1];
+    }
     // 4. switch() over it to decide on a course of action.
-
+    switch (IR) {
+      case LDI:
+      /* code */
+        cpu->REG[operandA] = operandB;
+        cpu->PC += num_of_operands + 1;
+      break;
+      case PRN:
+        printf("%d\n", cpu->REG[operandA]);
+        cpu->PC += num_of_operands +1;
+        break;
+      case HLT:
+        exit(1);
+        break;
+      default:
+        break;
+    }
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
   }
@@ -76,10 +100,15 @@ void cpu_init(struct cpu *cpu)
 int cpu_ram_read(struct cpu *cpu) 
 {
   int curr = cpu->RAM[(cpu->PC)];
-  printf("%d\n here", curr);
   int operandA = cpu->RAM[(cpu->PC) + 1];
   int operandB = cpu->RAM[(cpu->PC) + 2];
-  
+  if (curr >= 128) {
+    return 2;
+  } else if (curr >= 64) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 void  cpu_ram_write(struct cpu *cpu)
